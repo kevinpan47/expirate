cf714da3ef2715fdf41a3a2cd32ceeafb6233072
kevinpan47 <kpan8055@gmail.com>
2021-09-18 13:57:19 -0400

    added node backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import GoogleLogin from 'react-google-login';
+import React from 'react';
 import FoodProgress from './components/Food/foodProgress';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <GoogleLogin
+          clientId="979224664250-cv9l23uv6pbu88iq6gomg2o154702cbv.apps.googleusercontent.com"
+          buttonText="Login"
+          cookiePolicy={'single_host_origin'}
+        />
         <FoodProgress
           name={"Kimchi"}
           buyDate={"10/2/21"}
---
+++ server/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
