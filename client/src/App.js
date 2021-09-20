207aecd4eb5f18050fbc14dbcbd2abbfa021383f
kevinpan47 <kpan8055@gmail.com>
2021-09-20 10:46:18 -0400

    fixed login

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,40 @@ import GoogleLogin, { GoogleLogout } from 'react-google-login';
 import React, {useState, useMemo} from 'react';
 import FoodProgress from './components/Food/foodProgress';
 import { AuthContext } from './authContext';
+import {useHistory} from 'react-router-dom';
 
 function App() {
 
+  const history = useHistory();
   const [user, setUser] = useState(null);
-
   const value = useMemo(() => ({user, setUser}), [user, setUser]);
+
+  const logout = () => {
+    window.location.reload();
+  }
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    setUser(response.googleId);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         {user ? (
-          <GoogleLogout/>
+          <GoogleLogout
+          clientId="979224664250-cv9l23uv6pbu88iq6gomg2o154702cbv.apps.googleusercontent.com"
+          buttonText="Logout"
+          onLogoutSuccess={logout}
+          />
         ):(
           <GoogleLogin
           clientId="979224664250-cv9l23uv6pbu88iq6gomg2o154702cbv.apps.googleusercontent.com"
           buttonText="Login"
           cookiePolicy={'single_host_origin'}
-          onSuccess={() => setUser("john")}
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          isSignedIn={true}
         />
         )}
         <FoodProgress
