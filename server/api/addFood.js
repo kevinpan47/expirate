9119758b028e9787390a50624eb425bd8c8bc1ea
kevinpan47 <kpan8055@gmail.com>
2022-02-25 18:00:09 -0500

    basic validator

--- server/api/addFood.js
+++ server/api/addFood.js
@@ -1,6 +1,24 @@
+const bodySchema = {
+    type: 'object',
+    required: ['userID'],
+    properties: {
+        userID: {type: 'string'},
+        foodName: {type: 'string'},
+        expires: {type: 'string'},
+        notes: {type: 'string'}
+    }
+}
+
+exports.schema = {
+    body: bodySchema
+}
+
 exports.addFood = (req, res) => {
     const item = req.body;
-    
+
+    let userID = req.body.userID ? req.body.userID : ""
+    console.log(req.body.userID)
+
 };
 
 
--- server/index.js
+++ server/index.js
@@ -2,16 +2,28 @@ const fastify = require('fastify')({
     logger: true
 })
 
+const Ajv = require('ajv');
 const addFood = require(`${__dirname}/api/addFood`);
+
+
+const ajv = new Ajv({
+    removeAdditional: true,
+    useDefaults: true,
+    allErrors: true
+})
+
+fastify.setValidatorCompiler(({schema, method, url, httpPart}) => {
+    return ajv.compile(schema);
+})
 
 fastify.get('/', function (request, reply) {
     reply.send({ hello: 'world' })
 })
 
-fastify.post('/add', addFood.addFood);
+fastify.post('/add', {schema: addFood.schema}, addFood.addFood);
 
 // Run the server!
-fastify.listen(3000, function (err, address) {
+fastify.listen(3001, function (err, address) {
 if (err) {
     fastify.log.error(err)
     process.exit(1)
